e233b7603f26e4a5950ad26fdf325808540db6e6
FlagArihant2000 <arihant.gaur@gmail.com>
2020-07-09 10:58:27 +0530

    reconstuction with small speckles

--- stereocalibrate.py
+++ stereocalibrate.py
@@ -3,9 +3,12 @@ import numpy as np
 
 
 def Reprojection3D(image, disparity, f, b):
-	#Q = np.array([[1, 0, 0, -2964/2], [0, 1, 0, -2000/2],[0, 0, 0, f],[0, 0, -1/b, 124.343/b]])
+	Q = np.array([[1, 0, 0, -2964/2], [0, 1, 0, -2000/2],[0, 0, 0, f],[0, 0, -1/b, -124.343/b]])
 	#Q = np.array([[1, 0, 0, -2964/2], [0, 1, 0, 2000/2],[0, 0, 0, f],[0, 0, 0, 1]])
-	Q = np.array([[1, 0, 0, -1244.772], [0, 1, 0, -1019.507],[0, 0, 0, f],[0, 0, -1/b, 124.343/b]])
+	#Q = np.array([[1, 0, 0, -1244.772], [0, 1, 0, -1019.507],[0, 0, 0, f],[0, 0, -1/b, 124.343/b]])
+	#Q = np.array([[ 1.00000000e+00, 0.00000000e+00, 0.00000000e+00, -6.68420120e+02], [ 0.00000000e+00, 1.00000000e+00, 0.00000000e+00, -5.09922611e+02], [ 0.00000000e+00,  0.00000000e+00, 0.00000000e+00, 1.98995544e+03], [ 0.00000000e+00, 0.00000000e+00, 1.00000000e+00, -0.00000000e+00]])
+
+	print(Q)
 	#Q = np.array([[1, 0, 0, 0], [0, -1, 0, 0],[0, 0, f * 0.05, 0],[0, 0, 0, 1]])
 	points = cv2.reprojectImageTo3D(disparity, Q)
 	mask = disparity > disparity.min()
@@ -41,7 +44,7 @@ cv2.namedWindow('disparity', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Left Image', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Right Image', cv2.WINDOW_NORMAL)
 
-K = np.array([[3979.911, 0, 1244.772], [0, 3979.911, 1019.507], [0, 0, 1]], dtype = np.float32)
+K = np.array([[3979.911, 0, 1369.115], [0, 3979.911, 1019.507], [0, 0, 1]], dtype = np.float32)
 D = np.zeros((5,1), dtype = np.float32)
 downscale = 2
 K[0,0] = K[0,0] / float(downscale)
@@ -95,16 +98,33 @@ imgLrec = cv2.remap(imgL, map1, map2, cv2.INTER_CUBIC)
 
 map3, map4 = cv2.initUndistortRectifyMap(K, D, R2, P2, (1482, 1000), cv2.CV_16SC2)
 imgRrec = cv2.remap(imgR, map3, map4, cv2.INTER_CUBIC)
+print(Q)
 
 max_disparity = 271
 min_disparity = -1
 num_disparities = max_disparity - min_disparity
 window_size = 5
 stereo = cv2.StereoSGBM_create(minDisparity = min_disparity, numDisparities = num_disparities, blockSize = 5, uniquenessRatio = 5, speckleWindowSize = 5, speckleRange = 5, disp12MaxDiff = 2, P1 = 8*3*window_size**2, P2 = 32*3*window_size**2)
+
+stereo2 = cv2.ximgproc.createRightMatcher(stereo)
+
+#lamb = 120000
+#sig = 1.2
+#visual_multiplier = 1.0
+#wls_filter = cv2.ximgproc.createDisparityWLSFilter(stereo)
+#wls_filter.setLambda(lamb)
+#wls_filter.setSigmaColor(sig)
+
 disparity = stereo.compute(imgLrec, imgRrec)
 #cv2.filterSpeckles(disparity, 0, 400, max_disparity - 5)
 _, disparity = cv2.threshold(disparity, 0, max_disparity * 16, cv2.THRESH_TOZERO)
 disparity = (disparity / 16).astype(np.uint8)
+
+#disparity2 = stereo2.compute(imgRrec, imgLrec)
+#cv2.filterSpeckles(disparity, 0, 400, max_disparity - 5)
+#_, disparity2 = cv2.threshold(disparity2, 0, max_disparity * 16, cv2.THRESH_TOZERO)
+#disparity2 = (disparity2 / 16).astype(np.uint8)
+#filteredImg = wls_filter.filter(disparity, imgL, None, disparity2)
 
 baseline = 193.001
 f = 3979.911
